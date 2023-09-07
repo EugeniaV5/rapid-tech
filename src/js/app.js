79665d8f15272c08e852985dc2e6c40890271f1e
Ievgeniia Vashchenko <dn050493vem@gmail.com>
2023-09-07 17:24:45 +0300

    adds header, hero and principles
    1) hero -- is not finished. Slider to be added.

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+// import { isWebp } from "./is-webp";
+// import * as modulesFunctions from "./modules/functions";
+// isWebp();
+// isWebp();
+
+function testWebPFunction() {
+  function testWebP(callback) {
+    let webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src =
+      "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  }
+  testWebP(function (support) {
+    let className = support === true ? "webp" : "no-webp";
+    document.documentElement.classList.add(className);
+  });
+}
+
+testWebPFunction();
+
+console.log("hello");
